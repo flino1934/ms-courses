f4b5d5be0f128c52e7669a709f94c46c14285d94
Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>
2024-11-05 11:03:45 -0300

    Create MS worker and endpoints return workers all and id
    Signed-off-by: Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>

---
+++ hr-worker/src/main/java/com/lino/hr_worker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.lino.hr_worker.repositories;
+
+import com.lino.hr_worker.entity.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
