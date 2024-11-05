19c2b49f5feec63d9b600f585063da8aea8a7745
Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>
2024-11-05 19:48:40 -0300

    Implementando o balanceamento de carga usando de forma provisoria o Ribbon
    Signed-off-by: Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>

--- hr-payroll/src/main/java/com/lino/hr_payroll/services/PaymentServices.java
+++ hr-payroll/src/main/java/com/lino/hr_payroll/services/PaymentServices.java
@@ -2,6 +2,7 @@ package com.lino.hr_payroll.services;
 
 import com.lino.hr_payroll.entities.Payment;
 import com.lino.hr_payroll.entities.Worker;
+import com.lino.hr_payroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,18 +14,11 @@ import java.util.Map;
 @Service//Vai esta toda nossa regra de negocio
 public class PaymentServices {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
     @Autowired
-    private RestTemplate restTemplate;
-
+    private WorkerFeignClient workerFeignClient;
     public Payment getPayment(long workerId, int days){
 
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-
-        Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}",Worker.class, uriVariables);
-
+        Worker worker = workerFeignClient.findById(workerId).getBody();
        return new Payment(worker.getName(), worker.getDailyIncome(),days);
     }
 }
