03fdb3b64cd0f8026c9c68b16f895702d2d376ad
Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>
2024-11-07 11:15:11 -0300

    Configurando o timeout e forçando ele
    Signed-off-by: Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>

--- hr-payroll/src/main/java/com/lino/hr_payroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/lino/hr_payroll/controllers/PaymentController.java
@@ -2,6 +2,7 @@ package com.lino.hr_payroll.controllers;
 
 import com.lino.hr_payroll.entities.Payment;
 import com.lino.hr_payroll.services.PaymentServices;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,18 @@ public class PaymentController {
     @Autowired
     private PaymentServices services;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 
         Payment payment = services.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+
+        Payment payment = new Payment("Bran", 400.0,days);
         return ResponseEntity.ok(payment);
 
     }
--- hr-worker/src/main/java/com/lino/hr_worker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/lino/hr_worker/controller/WorkerController.java
@@ -38,7 +38,12 @@ public class WorkerController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-    logger.info("PORT = "+env.getProperty("local.server.port"));
+        try{
+            Thread.sleep(3000L);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        logger.info("PORT = "+env.getProperty("local.server.port"));
 
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok().body(obj);
