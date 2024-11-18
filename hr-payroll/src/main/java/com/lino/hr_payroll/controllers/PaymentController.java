0553707313eed3d10c34628b1e177450b2c6d0a3
Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>
2024-11-18 11:38:37 -0300

    Configurando a porta aletoria para o payroll
    Signed-off-by: Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>

--- hr-payroll/src/main/java/com/lino/hr_payroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/lino/hr_payroll/controllers/PaymentController.java
@@ -3,7 +3,10 @@ package com.lino.hr_payroll.controllers;
 import com.lino.hr_payroll.entities.Payment;
 import com.lino.hr_payroll.services.PaymentServices;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/payments")
 public class PaymentController {
 
+    private static Logger logger = LoggerFactory.getLogger(PaymentController.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private PaymentServices services;
 
     @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        logger.info("PORT = "+env.getProperty("local.server.port"));
 
         Payment payment = services.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
@@ -27,6 +36,7 @@ public class PaymentController {
     }
 
     public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+
 
         Payment payment = new Payment("Bran", 400.0,days);
         return ResponseEntity.ok(payment);
